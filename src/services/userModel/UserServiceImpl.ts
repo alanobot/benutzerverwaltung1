5a3c868be330497f07e3b0be5b9deed55fdc919b
Alan Parmaksiz <alanparmaksiz@gmail.com>
2021-03-04 09:38:57 +0100

    dateien vorbereitet

---
+++ src/services/userModel/UserServiceImpl.ts
@@ -0,0 +1,13 @@
+import axiosApi from "./AxiosUtility";
+
+
+export default class UserServiceImpl {
+  // public async retrieveAll(): Promise<User[]> {
+  //   try {
+  //     const { data } = await axiosApi.get<User[]>("/users");
+  //     return data;
+  //   } catch (error) {
+  //     throw new Error(error);
+  //   }
+  // }
+}
